0da17649b59a4c8aac2f9327b3664090221c1935
Anshu Priya <117987455+anshupriya06@users.noreply.github.com>
2023-11-07 23:10:36 +0530

    basic projects while learning DSA in JAVA

---
+++ VariableAndDatatypes.java
@@ -0,0 +1,21 @@
+public class VariableAndDatatypes {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "tony stark";
+
+        System.out.println(name);
+
+        a = 50;
+
+        System.out.println(a);
+
+        a = b ;
+        System.out.println(a);
+
+    }
+}
